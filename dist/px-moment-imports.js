41db0dce86753301f0c0f6d45c5fa7ca569cd681
David Leonard <davidrobertleonard@gmail.com>
2018-04-30 16:04:00 -0700

    Move to Apache-2.0 license

--- dist/px-moment-imports.js
+++ dist/px-moment-imports.js
@@ -1,2 +1,19 @@
+/**
+ * @license
+ * Copyright (c) 2018, General Electric
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 'use strict';(function(){Polymer({is:'px-moment-imports',properties:{counterValue:{type:Number,value:0,notify:true}},handleClick:function handleClick(event,detail,sender){this.increment()},increment:function increment(){this.counterValue++}})})();
 //# sourceMappingURL=px-moment-imports.js.map
